30f4cf8fc94068d7d92383aa2a1535317d630e23
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-20 17:22:29 -0400

    add book index file

---
+++ src/BookIndex.jsx
@@ -0,0 +1,7 @@
+export function BookIndex() {
+  return (
+    <div>
+      <h1>All Books</h1>
+    </div>
+  );
+}
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,10 @@
+import { BookIndex } from "./BookIndex";
+
 export function Content() {
   return (
     <div className="container-fluid">
       <h1>Welcome to The Book Manager</h1>
+      <BookIndex />
     </div>
   );
 }
