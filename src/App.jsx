b6d10fcccdeb51caf8cb18b0fec31adfb8be8b1a
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-20 17:03:25 -0400

    add content file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { Content } from "./Content";
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <Content />
         <Footer />
       </div>
     </>
---
+++ src/Content.jsx
@@ -0,0 +1,7 @@
+export function Content() {
+  return (
+    <div>
+      <h1>Welcome to The Book Management App</h1>
+    </div>
+  );
+}
