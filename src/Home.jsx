12d047e7d7327c5c06aed4c4b7fdb6f932a8a068
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-20 18:04:16 -0400

    add image to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ export function Home() {
   return (
     <div>
       <h1>Welcome to The Book Manager</h1>
+      <img src="https://img.icons8.com/?size=512&id=riWDux8q2OOr&format=png" alt="" />
     </div>
   );
 }
