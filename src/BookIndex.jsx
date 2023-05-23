037940298d3c82745ebafa290252d644894a9f4f
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-23 02:58:45 -0400

    render all books on page

--- src/BookIndex.jsx
+++ src/BookIndex.jsx
@@ -1,8 +1,24 @@
+import { ALL_BOOKS } from "./Queries";
+import { useQuery } from "@apollo/client";
+
 export function BookIndex() {
+  const { loading, error, data } = useQuery(ALL_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <>
-      <div>
-        <h1>All Books</h1>
+      <div className="">
+        <h1 className="m-5">Book Collection</h1>
+        {data.books.map((book) => (
+          <div className="card" key={book.id}>
+            <h2 className="mt-5">{book.title}</h2>
+            <img id="book-img" src={book.image} alt="" />
+            <h4 className="mt-1">{book.author}</h4>
+            <p>{book.publicationYear}</p>
+          </div>
+        ))}
       </div>
     </>
   );
