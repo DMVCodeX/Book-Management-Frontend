6fe33af3b7a759202725e2f86e9c077d297bf1d4
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-23 01:45:45 -0400

    integrate apollo client

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import client from "./Apollo.jsx";
+import { ApolloProvider } from "@apollo/client";
 import "./index.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
