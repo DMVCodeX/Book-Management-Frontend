a43d70476f9eb6ebe0c11c2ffaffeffa1762bc6f
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-23 16:43:32 -0400

    add redirects file for single page app

--- src/Apollo.jsx
+++ src/Apollo.jsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://book-manager-api.onrender.com/",
   cache: new InMemoryCache(),
 });
 
