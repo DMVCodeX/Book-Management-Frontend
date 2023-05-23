ce1fc366ea793885992e509586b4cbd133cdc1ef
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-23 02:31:43 -0400

    define queries

---
+++ src/Queries.jsx
@@ -0,0 +1,71 @@
+import { gql } from "@apollo/client";
+
+export const ALL_BOOKS = gql`
+  query AllBooks {
+    books {
+      id
+      title
+      author
+      publicationYear
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const GET_BOOK = gql`
+  query GetBook($id: Int!) {
+    book(id: $id) {
+      id
+      title
+      author
+      publicationYear
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const CREATE_BOOK = gql`
+  mutation CreateBook($title: String!, $author: String!, $publicationYear: Int!, $image: String) {
+    createBook(title: $title, author: $author, publicationYear: $publicationYear, image: $image) {
+      id
+      title
+      author
+      publicationYear
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const UPDATE_BOOK = gql`
+  mutation UpdateBook($id: Int!, $title: String, $author: String, $publicationYear: Int, $image: String) {
+    updateBook(id: $id, title: $title, author: $author, publicationYear: $publicationYear, image: $image) {
+      id
+      title
+      author
+      publicationYear
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation DeleteBook($id: Int!) {
+    deleteBook(id: $id) {
+      id
+      title
+      author
+      publicationYear
+      image
+      createdAt
+      updatedAt
+    }
+  }
+`;
