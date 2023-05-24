aa6826df36df48c168808004f5148f97e6ff1791
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-23 23:17:09 -0400

    book deatils file is now working and able to render a book by id

--- src/BookDetails.jsx
+++ src/BookDetails.jsx
@@ -1,3 +1,34 @@
-export function BookDetails() {
-  return <div></div>;
+import { useQuery } from "@apollo/client";
+import { GET_BOOK } from "./Queries";
+
+// eslint-disable-next-line react/prop-types
+export function BookDetails({ bookId }) {
+  const { loading, error, data } = useQuery(GET_BOOK, {
+    variables: { id: bookId },
+  });
+
+  if (loading)
+    return (
+      <img
+        src="https://blog.hubspot.com/hs-fs/hubfs/CSS%20infinite%20loading%20animation%20example.gif?width=1500&name=CSS%20infinite%20loading%20animation%20example.gif"
+        alt="..."
+      />
+    );
+  if (error) return <p>Error: {error.message}</p>;
+
+  const book = data.book;
+
+  return (
+    <div>
+      <h1>Book Details</h1>
+      <img src={book.image} alt="" />
+      <h3>Title: {book.title}</h3>
+      <h4>Author: {book.author}</h4>
+      <p>Publication Year: {book.publicationYear}</p>
+      <p>Created At: {book.createdAt}</p>
+      <p>Updated At: {book.updatedAt}</p>
+    </div>
+  );
 }
+
+export default BookDetails;
--- src/BookList.jsx
+++ src/BookList.jsx
@@ -4,19 +4,34 @@ import { useQuery } from "@apollo/client";
 export function BookList() {
   const { loading, error, data } = useQuery(ALL_BOOKS);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <img
+        src="https://blog.hubspot.com/hs-fs/hubfs/CSS%20infinite%20loading%20animation%20example.gif?width=1500&name=CSS%20infinite%20loading%20animation%20example.gif"
+        alt="Loading..."
+      />
+    );
   if (error) return <p>Error: {error.message}</p>;
 
   return (
     <>
-      <div className="">
-        <h1 className="m-5">Book Collection</h1>
+      <h1 className="m-5">Book Collection</h1>
+      <div className="" id="book-index">
         {data.books.map((book) => (
           <div className="" key={book.id}>
-            <h2 className="m-5">{book.title}</h2>
+            <h3 className="m-5">{book.title}</h3>
             <img id="book-img" src={book.image} alt="" />
-            <h4 className="mt-3">{book.author}</h4>
+            <p className="mt-3">{book.author}</p>
             <p>{book.publicationYear}</p>
+
+            <button
+              type="button"
+              value={book.id}
+              className="btn btn-outline-info btn-lg m-3 d-grid gap-2 col-6 mx-auto"
+            >
+              {" "}
+              Open {book.title}{" "}
+            </button>
           </div>
         ))}
       </div>
