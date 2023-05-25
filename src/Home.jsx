ee54e08fed9b1669d9df487ebae8c85b6dfc0976
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-25 13:58:22 -0400

    more style edits

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,14 @@
 export function Home() {
   return (
     <div>
-      <h1>Welcome to The Book Manager</h1>
-      <img src="https://img.icons8.com/?size=512&id=riWDux8q2OOr&format=png" alt="" />
+      <h1 className="m-3">Welcome to The Book Manager</h1>
+
+      <p>Click the image to browse our book collection.</p>
+      <img src="https://img.icons8.com/?size=1x&id=19044&format=png" alt="" />
+      <br />
+      <a href="/booklist">
+        <img src="https://img.icons8.com/?size=512&id=riWDux8q2OOr&format=png" alt="" />
+      </a>
     </div>
   );
 }
